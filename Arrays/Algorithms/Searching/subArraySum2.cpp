383dee879fa0b9f6726403a9fab2f22ba5aa8d7d
Manoj <manoj09633@gmail.com>
2022-11-20 13:36:57 +0530

    sub array sum calculation - prefix sum method

--- Arrays/Algorithms/Searching/subArraySum2.cpp
+++ Arrays/Algorithms/Searching/subArraySum2.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
 
-// Sub array sum calculation in brute force method
-// O(N^3)
+// Sub array sum calculation using prefix sum
+// O(N^2)
 int subArraySum(int n, int arr[])
 {
-    int largestSum = 0;
+    int prefix[10] = {0}, largestSum = 0;
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = i; j < n; j++)
         {
-            int subArraySum = 0;
-            for (int k = i; k <= j; k++)
-            {
-                subArraySum += arr[k];
-            }
+            int subArraySum = i > 0 ? prefix[j] - prefix[i - 1] : prefix[j];
             largestSum = max(largestSum, subArraySum);
         }
     }
     return largestSum;
 }
+
 int main()
 {
     int arr[10], n;
@@ -31,4 +34,5 @@ int main()
         cin >> arr[i];
     }
     cout << "Largest sum of the subArray is: " << subArraySum(n, arr);
+    return 0;
 }
